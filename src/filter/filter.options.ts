ccb4a94e4f498adfacd03557f54716cdb1989b60
0410-Cheremnov <cheremnovkonst@gmail.com>
2022-08-21 19:24:50 +0600

    data filter fix

--- src/filter/filter.options.ts
+++ src/filter/filter.options.ts
@@ -32,11 +32,15 @@ export class FilterOptions {
   }
 
   public static async getOptions(data: Partial<FilterOptions>) {
+    if (data.dateFrom <= data.dateTo) {
+      const t = data.dateFrom;
+      data.dateFrom = data.dateTo;
+      data.dateTo = t;
+    }
     return Object.assign({},
       data.limit && {take: data.limit},
       data.skip && {skip: data.skip},
       data.dateTo && data.dateFrom && {where: {date: Between(data.dateFrom, data.dateTo)}},
       data.order && data.sort && {order: {[data.order]: data.order}}
-    )
-  }
-}
+  )
+}}
